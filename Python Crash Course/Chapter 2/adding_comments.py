ecdaf92e7f92ff8b549e1183e2f535b6fc0899fd
Charles Stevano <chrlstev26@gmail.com>
2024-06-12 00:30:56 +0700

    add 2-7 - 2-12

---
+++ Python Crash Course/Chapter 2/adding_comments.py
@@ -0,0 +1,19 @@
+# 2-9
+
+# Charles, 6/12/2024
+# This program will print the number 8 by using different arithmetic operators.
+
+print(4+4)
+print(10-2)
+print(2*4)
+print(16/2) 
+
+# 2-10
+
+#Charles, 6/12/2024
+#This program stores my favorite number and then it will reveal the favorite number in a message.
+
+favorite_number = 25
+message = 'My favorite number is: '
+
+print(f"{message}{favorite_number}")
