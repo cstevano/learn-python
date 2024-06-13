d2b728d1efef4960157c8b2f5c9e5c6e7213a407
Charles Stevano <chrlstev26@gmail.com>
2024-06-13 23:48:34 +0700

    add 4-1

---
+++ Python Crash Course/Chapter_4/pizzas.py
@@ -0,0 +1,9 @@
+pizzas = ['pepperoni', 'pineapple', 'cheese']
+
+for pizza in pizzas:
+    print(f'I like {pizza} pizza.')
+
+print(f'\nI love {pizzas[0]} sooo muchh')
+print(f'I love {pizzas[1]} sooo muchh')
+print(f'I love {pizzas[2]} sooo muchh')
+print('\nI really love pizza!')
