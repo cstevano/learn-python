5c92483c4f61f3d074249e123f6ae9cc2897b575
Charles Stevano <chrlstev26@gmail.com>
2024-06-15 00:04:30 +0700

    add 5-7

---
+++ Python_Crash_Course/Chapter_5/favorite_fruit.py
@@ -0,0 +1,16 @@
+favorite_fruits = ['banana', 'apple', 'orange']
+
+if 'banana' in favorite_fruits:
+    print('you really like banana')
+
+if 'orange' in favorite_fruits:
+    print('you really like orange')
+
+if 'apple' in favorite_fruits:
+    print('you really like apple')
+
+if 'durian' in favorite_fruits:
+    print('you really like durian')
+
+if 'watermelon' in favorite_fruits:
+    print('you really like watermelon')
