2a9615254b53c0bd2355367f8e1e77617d5957d5
Charles Stevano <chrlstev26@gmail.com>
2024-06-14 23:47:08 +0700

    add 5-5

---
+++ Python_Crash_Course/Chapter_5/alien_colors_3.py
@@ -0,0 +1,10 @@
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print('You just earn 5 points for shooting the alien')
+
+elif alien_color == 'yellow':
+    print('You just earn 10 points for shooting the alien')
+
+elif alien_color == 'red':
+    print('You just earn 15 points for shooting the alien')
