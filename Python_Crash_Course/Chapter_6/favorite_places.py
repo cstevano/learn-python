8ca24cf6defb0e852e04b27fbdfc3a4a984a38ae
Charles Stevano <chrlstev26@gmail.com>
2024-06-15 14:04:55 +0700

    add 6-9

---
+++ Python_Crash_Course/Chapter_6/favorite_places.py
@@ -0,0 +1,8 @@
+favorite_places = {
+    'Tokyo': 'Charles',
+    'Penjara': 'Doni',
+    'Jail': 'Indra',
+}
+
+for place, name in favorite_places.items():
+    print(f'{place} is {name}\'s favorite place.')
