62b2f4697203e9c8af083c2e114be3f23c640feb
Charles Stevano <chrlstev26@gmail.com>
2024-06-15 12:08:28 +0700

    add 6-1

---
+++ Python_Crash_Course/Chapter_6/person.py
@@ -0,0 +1,11 @@
+my_info = {
+    'f_name': 'Charles',
+    'l_name': 'Stevano',
+    'age': 18,
+    'city': 'Ohio',
+}
+
+print(my_info['f_name'])
+print(my_info['l_name'])
+print(my_info['age'])
+print(my_info['city'])
