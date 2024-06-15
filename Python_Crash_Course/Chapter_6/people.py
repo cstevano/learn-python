b80c3ba015eb9825d82b633910647770fcca2884
Charles Stevano <chrlstev26@gmail.com>
2024-06-15 13:56:03 +0700

    add 6-7

---
+++ Python_Crash_Course/Chapter_6/people.py
@@ -0,0 +1,25 @@
+me = {
+    'f_name': 'Charles',
+    'l_name': 'Stevano',
+    'age': 18,
+    'city': 'Ohio',
+}
+
+nobara = {
+    'f_name': 'Kugisaki',
+    'l_name': 'Nobara',
+    'age': 18,
+    'city': 'Tokyo',
+}
+
+gojo = {
+    'f_name': 'Gojo',
+    'l_name': 'Satoru',
+    'age': 25,
+    'city': 'Tokyo',
+}
+
+chara = [me, nobara, gojo]
+
+for name in chara:
+    print(name)
