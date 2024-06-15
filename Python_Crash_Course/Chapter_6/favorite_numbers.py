b163006336b93f0af48d6b3f1cca2ec3509ebd87
Charles Stevano <chrlstev26@gmail.com>
2024-06-15 12:19:31 +0700

    add 6-2

---
+++ Python_Crash_Course/Chapter_6/favorite_numbers.py
@@ -0,0 +1,11 @@
+favorite_numbers = {
+    'Charles': '25',
+    'Doni': '1',
+    'Salmanan': '12',
+    'Indra': '67'
+}
+
+print(f"Charles's favorite number is {favorite_numbers.get('Charles')}.")
+print(f"Doni's favorite number is {favorite_numbers.get('Doni')}.")
+print(f"Salmanan's favorite number is {favorite_numbers.get('Salmanan')}.")
+print(f"Indra's favorite number is {favorite_numbers.get('Indra')}.")
