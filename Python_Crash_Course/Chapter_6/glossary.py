8960994915cb1a728e32f5e38d973340f5e1ca6a
Charles Stevano <chrlstev26@gmail.com>
2024-06-15 12:28:49 +0700

    add 6-3

---
+++ Python_Crash_Course/Chapter_6/glossary.py
@@ -0,0 +1,14 @@
+glossary = {
+    'for': 'looping until the condition is true',
+    'print': 'printing some information to the terminal',
+    'list': 'a sequence of value',
+    'variable': 'a label that is given to a value',
+    'IDE': 'a text editor that has a lot of built-in useful features'
+}
+
+print('Five programming words and its meaning:\n')
+print(f'for: {glossary.get('for')}')
+print(f'print: {glossary.get('print')}')
+print(f'list: {glossary.get('list')}')
+print(f'variable: {glossary.get('variable')}')
+print(f'IDE: {glossary.get('IDE')}')
