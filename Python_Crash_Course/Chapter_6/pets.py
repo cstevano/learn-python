bc80ca6efa9f1f0b0ef01dcd9c061698c711d6a4
Charles Stevano <chrlstev26@gmail.com>
2024-06-15 14:01:09 +0700

    add 6-8

---
+++ Python_Crash_Course/Chapter_6/pets.py
@@ -0,0 +1,8 @@
+pet_1 = {'type': 'dog', 'owner': 'Charles'}
+pet_2 = {'type': 'cat', 'owner': 'Charles'}
+pet_3 = {'type': 'tiger', 'owner': 'Charles'}
+
+pets = [pet_1, pet_2, pet_3]
+
+for pet in pets:
+    print(pet)
