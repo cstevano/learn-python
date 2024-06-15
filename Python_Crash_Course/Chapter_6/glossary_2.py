d65d47b1e0216972cafbfd48334708e082f91ba1
Charles Stevano <chrlstev26@gmail.com>
2024-06-15 13:04:11 +0700

    add 6-4

---
+++ Python_Crash_Course/Chapter_6/glossary_2.py
@@ -0,0 +1,13 @@
+glossary = {
+    'for': 'looping until the condition is true',
+    'print': 'printing some information to the terminal',
+    'list': 'a sequence of value',
+    'variable': 'a label that is given to a value',
+    'IDE': 'a text editor that has a lot of built-in useful features',
+    'tuple': 'an immutable list',
+    'if': 'checking a specific code, if it returns "True", a code will be executed',
+    'dictionary': 'a key-value pair',
+}
+
+for word, meaning in glossary.items():
+    print(f'{word}: {meaning}')
