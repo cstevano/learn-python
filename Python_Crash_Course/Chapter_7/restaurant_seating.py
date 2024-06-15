803fb5ac3e4f01b9e8ca44e9599d17f0f498bc52
Charles Stevano <chrlstev26@gmail.com>
2024-06-15 20:07:52 +0700

    add 7-2

---
+++ Python_Crash_Course/Chapter_7/restaurant_seating.py
@@ -0,0 +1,6 @@
+people = int(input('how many people do you have? '))
+
+if people > 8:
+    print('Sorry to inform you that you will need to wait for few minutes.')
+else:
+    print('Your table is ready!')
