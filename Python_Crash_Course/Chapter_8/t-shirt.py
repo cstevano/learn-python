162977a44e601793e17e649a8ae3c60438374cca
Charles Stevano <chrlstev26@gmail.com>
2024-06-17 15:22:31 +0700

    Add 8-3

---
+++ Python_Crash_Course/Chapter_8/t-shirt.py
@@ -0,0 +1,10 @@
+def make_shirt(size, text):
+    print('\nThe size of the t-shirt is:')
+    print(size)
+
+    print('\nThe text that will be printed on t-shirt:')
+    print(text)
+
+
+make_shirt('XL', 'Keep a smile on your face')
+make_shirt(text='Practice makes perfect', size='L')
