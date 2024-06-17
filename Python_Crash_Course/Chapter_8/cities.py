25420e40bf01c4bf03ce116823029935b8264844
Charles Stevano <chrlstev26@gmail.com>
2024-06-17 15:22:47 +0700

    Add 8-5

---
+++ Python_Crash_Course/Chapter_8/cities.py
@@ -0,0 +1,7 @@
+def describe_city(city_name='Reykjavik', country='Iceland'):
+    print(f'{city_name.title()} is in {country.title()}')
+
+
+describe_city()
+describe_city('Bogor', 'Indonesia')
+describe_city(country='Japan', city_name='Tokyo')
