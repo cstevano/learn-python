daac316992f70147c352c5f715d1093be7993d52
Charles Stevano <chrlstev26@gmail.com>
2024-06-17 15:22:39 +0700

    Add 8-4

---
+++ Python_Crash_Course/Chapter_8/large_shirts.py
@@ -0,0 +1,11 @@
+def make_shirt(size='L', text='I love Python'):
+    print('\nThe size of the t-shirt is:')
+    print(size)
+
+    print('\nThe text that will be printed on t-shirt:')
+    print(text)
+
+
+make_shirt()
+make_shirt('M')
+make_shirt('XS', 'Skibidi')
