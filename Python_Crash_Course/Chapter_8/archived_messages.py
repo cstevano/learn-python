41c4de8b335f3d5561fcdea95e3a24ef61a4507a
Charles Stevano <chrlstev26@gmail.com>
2024-06-17 16:51:53 +0700

    Add 8-11

---
+++ Python_Crash_Course/Chapter_8/archived_messages.py
@@ -0,0 +1,19 @@
+def send_messages(messages, sent_messages):
+    while messages:
+        message_to_sent = messages.pop()
+        print(f'Message to be sent: {message_to_sent}')
+        sent_messages.append(message_to_sent)
+
+
+def show_message(sent_messages):
+    for message in sent_messages:
+        print(message)
+
+
+messages = ['hi', 'ok', 'wow']
+sent_messages = []
+
+send_messages(messages[:], sent_messages)
+
+print(messages)
+print(sent_messages)
